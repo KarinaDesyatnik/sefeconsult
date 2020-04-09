cf816266e784bfb70d11cf4c85dcc8b6c5c35219
karinadesyatnik <karinadesyatnik27@gmail.com>
2020-04-09 18:24:28 +0300

    blog links

--- js/main.js
+++ js/main.js
@@ -49,6 +49,83 @@ $('.header').removeClass("sticky");
 }
 });
 
-//-------end sticky header--------
-    
+//-------end sticky header-------- 
+
 });
+
+$(document).ready(function(){
+            /*ПРОВЕРЯЕМ НАЖАТА ЛИ КНОПКА ОТПРАВКИ*/
+            $('#btn_submit').click(function(){
+                // собираем данные с формы
+               var user_name    = $('#name').val();
+                var user_email   = $('#email').val();
+                var text_comment = $('#text_comment').val();
+                // отправляем данные
+                $.ajax({
+                    url: "send.php", // куда отправляем
+                    type: "post", // метод передачи
+                    data: { // что отправляем
+                        "name":    user_name,
+                        "email":   user_email,
+                        "text_comment": text_comment
+                    },
+                    error:function(){
+                        $("#erconts").html("Произошла ошибка!");
+                        $("#erconts").css("color", "red");
+                }, 
+                    /* если произойдет ошибка в элементе с id erconts выведится сообщение*/                 
+                    beforeSend: function() {                     
+                        $("#erconts").html("Отправляем данные..."); 
+                                        
+                    },                 
+                    success: function(result){                     
+                        /* В случае удачной обработки и отправки выполнится следующий код*/                     
+                        $('#erconts').html(result);  
+                        $('#erconts').css('color', 'green');                   
+                                       
+                    }  
+                });
+            });
+        });
+
+
+$(document).ready(function(){
+            /*ПРОВЕРЯЕМ НАЖАТА ЛИ КНОПКА ОТПРАВКИ*/
+            $('#btn_submit-lv').click(function(){
+                // собираем данные с формы
+               var user_name    = $('#name').val();
+                var user_email   = $('#email').val();
+                var text_comment = $('#text_comment').val();
+                // отправляем данные
+                $.ajax({
+                    url: "send-lv.php", // куда отправляем
+                    type: "post", // метод передачи
+                    data: { // что отправляем
+                        "name":    user_name,
+                        "email":   user_email,
+                        "text_comment": text_comment
+                    },
+                    error:function(){
+                        $("#erconts").html("Ir radusies kļūda!");
+                        $("#erconts").css("color", "red");
+                }, 
+                    /* если произойдет ошибка в элементе с id erconts выведится сообщение*/                 
+                    beforeSend: function() {                     
+                        $("#erconts").html("Notiek datu nosūtīšana ..."); 
+                                        
+                    },                 
+                    success: function(result){                     
+                        /* В случае удачной обработки и отправки выполнится следующий код*/                     
+                        $('#erconts').html(result);  
+                        $('#erconts').css('color', 'green');                   
+                                       
+                    }  
+                });
+            });
+        });
+
+
+if(window.innerWidth < 780) {
+$('.contact__phone a').attr("href", "tel:+371 29 44 89 92");
+
+}
